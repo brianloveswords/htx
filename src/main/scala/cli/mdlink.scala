5a4514f44bed42964c4b5972bfa2c896301c732c
Brian J Brennan <brianloveswords@gmail.com>
2021-08-23 01:59:04 -0400

    can get titles

--- src/main/scala/cli/mdlink.scala
+++ src/main/scala/cli/mdlink.scala
@@ -10,7 +10,7 @@ import org.http4s.ParseFailure
 import java.net.URL
 import org.http4s.Status
 import org.http4s.Headers
-import org.typelevel.ci.CIString
+import org.typelevel.ci.*
 import cats.MonadError
 import cats.Monad
 
@@ -39,41 +39,41 @@ private def parseUri(uri: String): IO[Uri] =
 
   Uri.fromString(uri).fold(toError, IO.pure).flatMap(confirm)
 
-def getHeader(
-    headers: Headers,
-    name: String,
-): Option[String] =
-  headers.get(CIString(name)) match
-    case Some(header) => Some(header.head.value)
-    case None         => None
+private def getHeader(name: String)(headers: Headers): Option[String] =
+  headers.get(CIString(name)).map(_.head.value)
+
+private def showRedirect(uri: Uri): IO[Unit] =
+  Console[IO].errorln(s"following redirect: $uri")
 
 object mdlink extends IOApp:
+  def getLocationHeader = getHeader("location")
   val client = JavaNetClientBuilder[IO].create
 
-  def get(uri: Uri): IO[String] =
-    client.get[String](uri) { resp =>
+  def getHtml(uri: Uri): IO[(String, Uri)] =
+    client.get(uri) { resp =>
       resp.status match
-        case Status.Ok => resp.as[String]
+        case Status.Ok => resp.as[String].map((_, uri))
         case Status.MovedPermanently =>
           val headers = resp.headers
           lazy val error = IO.raiseError(MissingLocationHeader(uri, headers))
-          getHeader(headers, "location")
+          getLocationHeader(headers)
             .fold(error)(IO.pure)
             .flatMap(parseUri)
-            .flatTap(uri => Console[IO].errorln(s"following redirect: $uri"))
-            .flatMap(get)
+            .flatTap(showRedirect)
+            .flatMap(getHtml)
         case status => IO.raiseError(UnexpectedStatus(uri, status))
     }
 
   def run(args: List[String]): IO[ExitCode] = for
-    url <- parseUri(args)
-    _ <- Console[IO].errorln(s"url: $url")
-    html <- get(url)
+    uri <- parseUri(args)
+    _ <- Console[IO].errorln(s"url: $uri")
+    result <- getHtml(uri)
+    (html, uri) = result
     soup = PureSoup(html)
     selector = Selector.unsafe("title")
     element = soup.extract(selector)
-    title = element.fold("<not found>")(_.text)
-    _ <- Console[IO].errorln(s"$title")
+    title = element.fold("<title not found>")(_.text)
+    _ <- Console[IO].print(s"[$title]($uri)")
   yield ExitCode.Success
 
 // NOTE: since I'm only making one http request, this ends up being slower.
