b5eec2a102e5699d209d11a827bf944f1ea2c444
Brian J Brennan <brianloveswords@gmail.com>
2021-08-24 23:39:06 -0400

    rearranging scripts

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+## this is mounted in the builder image and intended to run from there
+
 set -euo pipefail
 
-sbt cli/nativeImage
-cp htx-cli/target/native-image/cli /wormhole/htx.amd64.linux.static
-upx /wormhole/htx.amd64.linux.static
+function main() {
+    if [ "$PWD" = "/workspace" ]; then
+        sbt cli/nativeImage
+        cp htx-cli/target/native-image/cli /wormhole/htx.amd64.linux.static
+        upx /wormhole/htx.amd64.linux.static
+        exit 0
+    fi
+    echo "must be run from /workspace"
+    exit 1
+}
+
+main
