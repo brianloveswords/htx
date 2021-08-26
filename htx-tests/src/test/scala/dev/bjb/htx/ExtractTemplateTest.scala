4ea933140b0fd585db031e4c975332aaa97d57a4
Brian J Brennan <brianloveswords@gmail.com>
2021-08-25 20:45:00 -0400

    wip. got a lot of errors working

--- htx-tests/src/test/scala/dev/bjb/htx/ExtractTemplateTest.scala
+++ htx-tests/src/test/scala/dev/bjb/htx/ExtractTemplateTest.scala
@@ -6,24 +6,63 @@ import scala.util.control.NoStackTrace
 // htx example.com author:"meta[property='twitter:title']" "[{h1.title}]({@})"
 
 enum ExtractTemplateError extends NoStackTrace:
-  case EmptyTemplate
+  case NoReplacements(template: Template)
+  case UnusedExtracts(extractors: Map[String, Extract])
 
 case class ExtractTemplate private (
-    extractors: Seq[Extract],
+    extractorMap: Map[String, Extract],
     template: Template,
 )
 case object ExtractTemplate:
+  import ExtractTemplateError.*
+
+  lazy val extractRe = raw"\{(.*?)\}".r
   def from(
-      extractors: Seq[Extract],
+      extractors: Map[String, Extract],
       template: Template,
-  ): Either[ExtractTemplateError, ExtractTemplate] =
-    Left(ExtractTemplateError.EmptyTemplate)
+  ): Either[ExtractTemplateError, ExtractTemplate] = for
+    tpl <- Right(template.value)
+    matches <- Right(extractRe.findAllMatchIn(tpl).map(m => m.group(1)).toList)
+    ex <-
+      if matches.lengthIs == 0 then Left(NoReplacements(template))
+      else mergeMatches(matches, extractors)
+  yield ExtractTemplate(ex, template)
+
+  private def mergeMatches(
+      matches: List[String],
+      extractors: Map[String, Extract],
+  ): Either[ExtractTemplateError, Map[String, Extract]] = for
+    ex <- Right(extractors)
+    keys = extractors.keySet
+    unused = keys.diff(matches.toSet)
+    _ <-
+      if unused.sizeIs == 0 then Right(extractors)
+      else Left(UnusedExtracts(ex.view.filterKeys(unused.contains(_)).toMap))
+  yield ???
 
 class ExtractTemplateTest extends CommonSuite:
-  test("parse empty pair") {
+  import ExtractTemplateError.*
+
+  test("no replacements") {
+    val template = Template("cool")
     val result = ExtractTemplate.from(
-      extractors = List(),
-      template = Template("cool"),
+      extractors = Map(),
+      template = template,
     )
-    assertEquals(result, Left(ExtractTemplateError.EmptyTemplate))
+    assertEquals(result, Left(NoReplacements(template)))
+  }
+
+  test("unused extracts") {
+    val template = Template("x{author}x")
+    val title = ("title" -> Extract(Selector.unsafe("h1.title")))
+    val author = ("author" -> Extract(
+      Selector.unsafe("meta[name='author']"),
+      Some("content"),
+      Some("Unknown Author"),
+    ))
+    val result = ExtractTemplate.from(
+      extractors = Map(title, author),
+      template = template,
+    )
+    assertEquals(result, Left(UnusedExtracts(Map(title))))
   }
