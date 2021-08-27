52a2b3bec65c14842d701fa45ccba0ae5e435455
Brian J Brennan <brianloveswords@gmail.com>
2021-08-27 02:07:15 -0400

    now with fallback

--- htx-tests/src/test/scala/dev/bjb/htx/SelectorExtractor.scala
+++ htx-tests/src/test/scala/dev/bjb/htx/SelectorExtractor.scala
@@ -11,32 +11,43 @@ import cats.data.Validated.Valid
 import cats.effect.IO
 
 case class SelectorExtractor(template: String):
-  val eval = TemplateEvaluator(template)
-  val unsafeSelectors = tpl.patterns.map(p => (p, Selector.unsafe(p)))
+  lazy val tpl = TemplateEvaluator(template)
+  lazy val unsafeSelectors = tpl.patterns.map(p => (p, Selector.unsafe(p)))
+  lazy val empty: ValidatedNel[Throwable, Set[Selector]] = Set().validNel
+  lazy val validatedSelectors = tpl.patterns.foldLeft(empty)((acc, p) =>
+    acc.combine(Selector(p).toValidatedNel.map(Set(_))),
+  )
 
   def eval(html: String): IO[List[String]] =
     val soup = PureSoup(html)
     val empty: Map[String, List[String]] = Map.empty
     unsafeSelectors
       .map { (p, selector) =>
-        IO((p, soup.extract(selector).map(_.text)))
+        IO(soup.extract(selector).map(_.text)) map { extracts =>
+          (p, if extracts.sizeIs == 0 then List(s"<missing: $p") else extracts)
+        }
       }
       .toList
       .parSequence
       .map(_.toMap)
-      .flatMap(eval.eval(_))
-
-val tpl = TemplateEvaluator("{title} by {.author}")
-val empty: ValidatedNel[Throwable, Set[Selector]] = Set().validNel
-val validatedSelectors = tpl.patterns.foldLeft(empty)((acc, p) =>
-  acc.combine(Selector(p).toValidatedNel.map(Set(_))),
-)
-val unsafeSelectors = tpl.patterns.map(Selector.unsafe(_))
+      .flatMap(tpl.eval(_))
 
 class SelectorExtractorTest extends CommonSuite:
   test("title from html") {
     val ex = SelectorExtractor("{title}")
     ex.eval("<title>cool</title>") map { result =>
       assertEquals(result, List("cool"))
+    }
+  }
+  test("multiple matching elements") {
+    val html = """
+    |<title>site</title>
+    |<h2 class=author>A1</h2>
+    |<h2 class=author>A2</h2>
+    """.stripMargin
+
+    val ex = SelectorExtractor("{title}: {.author}")
+    ex.eval(html) map { result =>
+      assertEquals(result, List("site: A1", "site: A2"))
     }
   }
