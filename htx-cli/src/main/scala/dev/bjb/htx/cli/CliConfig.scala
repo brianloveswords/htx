9b73551191177b7d7e859f6ad88747a0e4f5c400
Brian J Brennan <brianloveswords@gmail.com>
2021-08-28 17:20:08 -0400

    URI reader going

--- htx-cli/src/main/scala/dev/bjb/htx/cli/CliConfig.scala
+++ htx-cli/src/main/scala/dev/bjb/htx/cli/CliConfig.scala
@@ -1,9 +1,25 @@
 package dev.bjb.htx.cli
 
 import org.http4s.Uri
+import scopt.OParser
+import scopt.Read
 
 case class CliConfigRaw(
     num: Option[Int] = None,
-    uri: String,
+    uri: Option[Uri] = None,
     kwargs: Map[String, String] = Map.empty,
 )
+
+given Read[Uri] = Read.reads { s =>
+  val base =
+    if s.startsWith("http") then s
+    else if !s.contains("://") then "https://" + s
+    else throw new IllegalArgumentException(s"Invalid URI")
+  Uri.fromString(base) match
+    case Right(uri) =>
+      uri.scheme match
+        case Some(Uri.Scheme.http) | Some(Uri.Scheme.https) => uri
+        case _ =>
+          throw new IllegalArgumentException(s"Scheme must be http(s)")
+    case Left(err) => throw new IllegalArgumentException(err.toString)
+}
--- htx-tests/src/test/scala/dev/bjb/htx/CliConfigTest.scala
+++ htx-tests/src/test/scala/dev/bjb/htx/CliConfigTest.scala
@@ -1,7 +1,8 @@
 package dev.bjb.htx
 
-import cli.*
+import cli.{*, given}
 import scopt.OParser
+import org.http4s.Uri
 
 class CliConfigTest extends CommonSuite:
   val builder = OParser.builder[CliConfigRaw]
@@ -12,13 +13,27 @@ class CliConfigTest extends CommonSuite:
       head("htx", "1.0.0"),
       opt[Int]('k', "num")
         .validate(k =>
-          if k > 0 then Left("num must be greater than 0")
-          else Right(()),
+          if k > 0 then success
+          else failure("num must be >0"),
         )
         .action((k, c) => c.copy(num = Some(k)))
         .text("How many matches to return. When not set, it is unlimited"),
+      arg[Uri]("<uri>")
+        .action((uri, c) => c.copy(uri = Some(uri)))
+        .text(
+          "URI to pull contents. - for contents on stdin; @ for URIs",
+        ),
     )
 
   test("hello") {
+    val args = Seq("--num", "1", "example")
+    val (result, effects) =
+      OParser.runParser(parser1, args, CliConfigRaw())
+    result.foreach { config =>
+      config.uri.foreach { uri =>
+        println(uri.toString)
+      }
+    }
+    println(effects)
     assert(true)
   }
