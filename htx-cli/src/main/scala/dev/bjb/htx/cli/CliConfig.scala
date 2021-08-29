76d152201a6aa12015831f5308457b26b54779ff
Brian J Brennan <brianloveswords@gmail.com>
2021-08-28 20:30:39 -0400

    wip. return either from parse

--- htx-cli/src/main/scala/dev/bjb/htx/cli/CliConfig.scala
+++ htx-cli/src/main/scala/dev/bjb/htx/cli/CliConfig.scala
@@ -3,6 +3,10 @@ package dev.bjb.htx.cli
 import org.http4s.Uri
 import scopt.OParser
 import scopt.Read
+import dev.bjb.htx.TemplateEvaluator
+import scala.util.control.NoStackTrace
+import scopt.DefaultOEffectSetup
+import scopt.OEffect
 
 enum InputType:
   case Link(uri: Uri)
@@ -12,8 +16,63 @@ enum InputType:
 case class CliConfigRaw(
     num: Option[Int] = None,
     uri: Option[Uri] = None,
-    template: Option[String] = None,
+    template: Option[TemplateEvaluator] = None,
 )
+
+case class ArgumentsError(msg: String) extends NoStackTrace:
+  override def getMessage: String = msg
+
+object CliConfigRaw:
+  val builder = OParser.builder[CliConfigRaw]
+  def parse(args: Seq[String]): Either[ArgumentsError, CliConfigRaw] =
+    import builder.*
+    val p = OParser.sequence(
+      programName("htx"),
+      head("htx", "1.0.0"),
+      opt[Int]('k', "num")
+        .validate { k =>
+          if k > 0 then success
+          else failure("num must be >0")
+        }
+        .action((k, c) => c.copy(num = Some(k)))
+        .text("How many matches to return. When not set, it is unlimited"),
+      arg[Uri]("<uri>")
+        .action((uri, c) => c.copy(uri = Some(uri)))
+        .text(
+          "URI to pull contents. - for contents on stdin; @ for URIs",
+        ),
+      arg[TemplateEvaluator]("<template>")
+        .action((template, c) => c.copy(template = Some(template)))
+        .text(
+          "extraction template. Format: {<css> [ |> fn1 |> fn2 ] }",
+        ),
+      checkConfig { c =>
+        if c.uri.isEmpty then failure("uri must be set")
+        else success
+      },
+    )
+    val (result, effects) = OParser.runParser(p, args, CliConfigRaw())
+    result match
+      case Some(config) => Right(config)
+      case _            => Left(ArgumentsError(getMessage(effects)))
+
+  def getMessage(effects: List[OEffect]): String =
+    var message = ""
+    OParser.runEffects(
+      effects,
+      new DefaultOEffectSetup {
+        override def displayToOut(msg: String): Unit =
+          message += msg + "\n"
+        override def displayToErr(msg: String): Unit =
+          message += msg + "\n"
+        override def reportError(msg: String): Unit =
+          displayToErr("Error: " + msg)
+        override def reportWarning(msg: String): Unit =
+          displayToErr("Warning: " + msg)
+        override def terminate(exitState: Either[String, Unit]): Unit = ()
+      },
+    )
+    message
 
 given Read[Uri] = Read.reads { s =>
   val base =
@@ -27,4 +86,10 @@ given Read[Uri] = Read.reads { s =>
         case _ =>
           throw new IllegalArgumentException(s"Scheme must be http(s)")
     case Left(err) => throw new IllegalArgumentException(err.toString)
+}
+
+given Read[TemplateEvaluator] = Read.reads { s =>
+  try TemplateEvaluator.unsafe(s)
+  catch
+    case err: Throwable => throw new IllegalArgumentException(err.getMessage)
 }
--- htx-tests/src/test/scala/dev/bjb/htx/CliConfigTest.scala
+++ htx-tests/src/test/scala/dev/bjb/htx/CliConfigTest.scala
@@ -5,44 +5,9 @@ import scopt.OParser
 import org.http4s.Uri
 
 class CliConfigTest extends CommonSuite:
-  val builder = OParser.builder[CliConfigRaw]
-  val parser1 =
-    import builder.*
-    OParser.sequence(
-      programName("htx"),
-      head("htx", "1.0.0"),
-      opt[Int]('k', "num")
-        .validate(k =>
-          if k > 0 then success
-          else failure("num must be >0"),
-        )
-        .action((k, c) => c.copy(num = Some(k)))
-        .text("How many matches to return. When not set, it is unlimited"),
-      arg[Uri]("<uri>")
-        .action((uri, c) => c.copy(uri = Some(uri)))
-        .text(
-          "URI to pull contents. - for contents on stdin; @ for URIs",
-        ),
-      arg[String]("<template>")
-        .action((template, c) => c.copy(template = Some(template)))
-        .text(
-          "URI to pull contents. - for contents on stdin; @ for URIs",
-        ),
-      checkConfig { c =>
-        if c.uri.isEmpty then failure("uri must be set")
-        else success
-      },
-    )
-
   test("works with a full url and static template") {
-    val args = Seq("example")
-    val (result, effects) =
-      OParser.runParser(parser1, args, CliConfigRaw())
-
-    for config <- result
-    yield println(config)
-
-    OParser.runEffects(effects)
-
+    val args = Seq("example", "{@")
+    val result = CliConfigRaw.parse(args)
+    println(result)
     assert(true)
   }
